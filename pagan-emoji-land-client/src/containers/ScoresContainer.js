573f204366141e96f6520f3d9d0b91bab45fc385
KBENBENECK <kbenbeneck@gmail.com>
2021-01-31 16:57:00 -0500

    Create ScoresContainer.js

---
+++ pagan-emoji-land-client/src/containers/ScoresContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ScoresContainer extends Component {
+    render() {
+        return (
+            <div>
+                Fetch Rails DB!
+            </div>
+        )
+    }
+}
+
+export default ScoresContainer;
