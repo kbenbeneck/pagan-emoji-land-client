03c36ed39150d112747090e97bb958311ef75ca9
KBENBENECK <kbenbeneck@gmail.com>
2021-01-31 16:57:08 -0500

    Create HowToPlay.js

---
+++ pagan-emoji-land-client/src/components/NewGame/HowToPlay.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+
+export default class HowToPlay extends Component {
+    render() {
+        return (
+            <div>
+                <hr />
+                How To Play!
+
+                <p>
+                    <label>
+                        Object:
+                    </label>
+                    <br />
+                    Leave the most impact before reaching the New Moon during the Winter Solstice.
+                </p>
+                <p>
+                    <label>
+                        Leaving Impact:
+                    </label>
+                    <br />
+                    Impact is left when a player lands on any board space and remains until another element lands on that space. 
+                    <br />
+                    🪨   🪨   🪨   🜃
+                    💧  💧  💧  🜄
+                    🌀  🌀  🌀  🜁
+                    🔥  🔥  🔥  🜂
+                </p>
+                <p>
+                    <label>
+                        Moving:
+                    </label>
+                    <br />
+                    Click  <button>𖤐 Summon 𖤐</button> to reach into the macrocosm and reveal a celestial body or seasonal event.
+                </p>
+                <p>
+                    <label>
+                        &#127761;
+                        &#127762;
+                        &#127763;
+                        &#127764;
+                        &#127765;
+                        &#127766;
+                        &#127767;
+                        &#127768;
+                        &#127761;
+                    </label>
+                    <br />
+                    Move ahead to the next space that matches the moon phase.
+                </p>
+                <p>
+                    <ul>
+                        <li>♈ Aries ♈</li>                
+                        <li>♊ Gemini ♊</li>
+                        <li>♋ Cancer ♋</li>                    
+                        <li>♎ Libra ♎</li>                       
+                        <li>☿ Mercury ☿</li>                        
+                        <li>♃ Jupiter ♃</li>
+                        <li>♄ Saturn ♄</li>                      
+                        <li>♆ Neptune ♆</li>
+                        <li>☉ Sun ☉</li>                       
+                    </ul>                                   
+                    Move forward or back to the space that matches the astrological sign or planetary body.
+                </p>
+                <p>
+                    <ul>Fests:
+                        <li>Yule 🌲 ❄ </li>
+                        <li>Imbolc 🕯 🌱</li>
+                        <li>Ostara 🥚 &#128007;</li>
+                        <li>Beltane 🍃</li>
+                        <li>Litha 🌻</li>
+                        <li>Lughnasadh 🌽 </li>
+                        <li>Mabon 🍂</li>
+                        <li>Samhain 🎃 </li>
+                    </ul>
+                    Move forward to the begining of the festival or backward to the end of the festival. 
+                </p>
+            </div>
+        )
+    }
+}
