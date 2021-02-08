0be020a7dd60391b34a2662e89c0c22f929a15f1
KBENBENECK <kbenbeneck@gmail.com>
2021-02-08 10:09:11 -0500

    add signs, bodies, and summon button to grid

--- pagan-emoji-land-client/src/components/gameboard/Spaces.js
+++ pagan-emoji-land-client/src/components/gameboard/Spaces.js
@@ -12,6 +12,41 @@ export default class Spaces extends Component {
         const waningGibbous = <p>&#127766;</p>
         const lastQuarter = <p>&#127767;</p>
         const waningCrescent = <p>&#127768;</p>
+
+        const aries = <p>♈</p>
+        const taurus = <p>♉</p>
+        const gemini = <p>♊</p>
+        const cancer = <p>♋</p>
+        const leo = <p>♌</p>
+        const virgo = <p>♍</p>
+        const libra = <p>♎</p>
+        const scorpio = <p>♏</p>
+        const sagittarius = <p>♐</p>
+        const capricorn = <p>♑</p>
+        const aquarius = <p>♒</p>
+        const pisces = <p>♓</p>
+
+        const mercury = <p>☿</p>
+        const venus = <p>♀</p>
+        const earth = <p>♁</p>
+        const mars = <p>♂</p>
+        const jupiter = <p>♃</p>
+        const saturn = <p>♄</p>
+        const uranus = <p>⛢</p>
+        const neptune = <p>♆</p>
+        const basicMoon = <p>☽︎</p>
+        const sun = <p>☉</p>
+        const pluto = <p>♇</p>
+        //97-121
+        const signsBodies = [
+            aries, taurus, gemini, cancer, 
+            leo, virgo, libra, scorpio,
+            sagittarius, capricorn, aquarius, pisces,
+            saturn, uranus, neptune, mars, venus,
+            mercury, basicMoon, sun, mercury,
+            venus, pluto, jupiter
+
+        ]
         //8 phases
         const moonCycle = [
             newMoon,
@@ -40,13 +75,15 @@ export default class Spaces extends Component {
             newMoon
         ]
         
-        const spacesArr = blankSpaces.concat(moons)
+        const spacesArr = blankSpaces.concat(moons, signsBodies)
         const spaces = spacesArr.map((space, index) => <div className={`space-${index}`} key={index}>{space}</div>)
         
         return (
             <div className="spaces">
                 {spaces}
-                <Player playerOne={playerOne}/>         
+                <Player playerOne={playerOne}/>
+                
+                <button className="summon">𖤐 Summon 𖤐</button>         
             </div>
         )
     }
