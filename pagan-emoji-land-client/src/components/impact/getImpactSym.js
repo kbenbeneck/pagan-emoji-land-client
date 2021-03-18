d693957c498d7df000d28804cc1d83acbe9baf15
KBENBENECK <kbenbeneck@gmail.com>
2021-03-18 01:58:25 -0400

    Form & Impact
    ChoosePlayer(+s) now includes 4 players. Instead of selecting, the user now chooses by clicking ele buttons. On click, the state is controlled via filtering the symbols array against the user's choice and sending it through a switch statement setting expected state. When p1 exists, the submit button and confirmation statement are rendered. Clicking submit dispatches action CHOOSE_PLAYERS, available via props through connect.

---
+++ pagan-emoji-land-client/src/components/impact/getImpactSym.js
@@ -0,0 +1,19 @@
+export function getImpactSym(player) {
+    
+    
+    switch(player) {
+        
+        case '🜃': 
+            return '🌱'   
+        case '🜄':
+            return '💧'
+        case '🜁':
+            return '🌪'
+        case '🜂':
+            return '🔥'
+        default:
+            return null
+    }
+}
+
+            
--- pagan-emoji-land-client/src/components/players/ChoosePlayer.js
+++ pagan-emoji-land-client/src/components/players/ChoosePlayer.js
@@ -1,41 +1,112 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getImpactSym } from '../impact/getImpactSym';
 
 
 class ChoosePlayer extends Component {
     
     state = {  
-            
-        playerOne: '',        
-            
+        players: ["🜄","🜁","🜃","🜂"],  
+        playerOne: '', 
+        playerTwo: '',       
+        playerThree: '',       
+        playerFour: '',  
+        elementName: '',
     }
     
-    handleOnChange = event => {
-        this.setState({ playerOne: event.target.value })     
+    handleOnClick = event => {
+        event.preventDefault();
+        const selected = event.target.innerText
+        const selectedIndex = this.state.players.indexOf(selected)
+        const p1 = this.state.players[selectedIndex]
+        const nonPlayers = this.state.players.filter(function (e) {return e !== p1})
+        const p2 = nonPlayers[0]
+        const p3 = nonPlayers[1]
+        const p4 = nonPlayers[2]
+        //sep this func
+        switch(selected) {
+            case '🜄':
+                return this.setState({
+                    elementName: 'water',
+                    playerOne: p1,
+                    playerTwo: p2,
+                    playerThree: p3,
+                    playerFour: p4
+                });
+            case '🜁':
+                return this.setState({
+                    elementName: 'air',
+                    playerOne: p1,
+                    playerTwo: p2,
+                    playerThree: p3,
+                    playerFour: p4
+                });
+            case '🜃':
+                return this.setState({
+                    elementName: 'earth',
+                    playerOne: p1,
+                    playerTwo: p2,
+                    playerThree: p3,
+                    playerFour: p4
+                });
+            case '🜂':
+                return this.setState({
+                    elementName: 'fire',
+                    playerOne: p1,
+                    playerTwo: p2,
+                    playerThree: p3,
+                    playerFour: p4
+                });
+            default:
+                return null   
+        }   
     }
 
     handleOnSubmit(event) {
         event.preventDefault();
-        this.props.dispatch({ type: 'CHOOSE_PLAYER', payload: this.state.playerOne})
+        this.props.dispatch({ type: 'CHOOSE_PLAYERS', payload: this.state })
         this.setState({
-            playerOne: '',            
-        });
-            
+            playerOne: '', 
+            playerTwo: '',       
+            playerThree: '',       
+            playerFour: '',  
+            elementName: '',           
+        });      
     }
-    
-    render() {             
+
+    render() {  
+
+        const playerChoicesButtons = this.state.players.map((symbol, index) => 
+            <button 
+                key={index}
+                id={index}
+                onClick={this.handleOnClick} 
+                className={symbol + index}>
+                    {symbol}
+            </button>
+        );
+
+        
+        const s = `Do you wish to play as ${this.state.elementName} ?`
+        
         return (
             <div className="player-choices">                               
                 <form onSubmit={event => this.handleOnSubmit(event)}>
-                    <label>Choose an Element:</label>
-                    <select value={this.state.playerOne} onChange={this.handleOnChange}>
-                        <option value="🜄">🜄</option>
-                        <option value="🜁">🜁</option>
-                        <option value="🜃">🜃</option>
-                        <option value="🜂">🜂</option>
-                    </select>
-                   <button type="submit">Play</button>                                                 
-                </form>                                
+                   
+                    {/* render buttons from players state */}
+                    {playerChoicesButtons} 
+
+                    {/* submit only vis on p1 state change  */}
+                    {   (this.state.playerOne !== '')
+                        ? <button className="play-button"type="submit"><span>{this.state.playerOne}</span><span>{getImpactSym(this.state.playerOne)}</span></button>
+                        : null }
+
+                    {/* choose or play as _? */}
+                    {   (this.state.elementName !== '')
+                        ? <p className="confirm-player">{s}</p>
+                        : <p className="confirm-player">Choose a classical element.</p> } 
+                                            
+                </form>                          
             </div>
         )
     }
