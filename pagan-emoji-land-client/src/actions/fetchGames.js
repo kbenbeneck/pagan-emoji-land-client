4c7774a5fc575fd6409cc29cbef595ab19da7cde
KBENBENECK <kbenbeneck@gmail.com>
2021-04-01 18:08:39 -0400

    Move fetchGame and createGame action creators into actions directory.

--- pagan-emoji-land-client/src/actions/fetchGames.js
+++ pagan-emoji-land-client/src/actions/fetchGames.js
@@ -1,10 +1,15 @@
 export const fetchGames = () => {
+    console.log('b')
     return (dispatch) => {
+        console.log('c')
         dispatch({ type: 'LOADING_GAME_STATS'})
         fetch('http://localhost:3001/games').then(response => {
             return response.json()
         }).then(responseJSON => {
+            console.log('d')
             dispatch({ type: 'ADD_STATS', games: responseJSON})
         })
+        console.log('e')
     }
+    console.log('f')
 }
