7448494ca557849541b3cb26c9253465724f9654
KBENBENECK <kbenbeneck@gmail.com>
2021-04-01 18:06:41 -0400

    Move routes and navigation to App, leaving only the /play route in GameContainer

--- pagan-emoji-land-client/src/App.js
+++ pagan-emoji-land-client/src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
 import GameContainer from './containers/GameContainer';
+import APIContainer from './containers/APIContainer';
+import HowToPlay from './components/rules/HowToPlay'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link 
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      
-      <GameContainer />
-    </div>
+    
+      <Router>
+        <div className="App"> 
+
+          <nav>
+            <Link to="/">Home</Link><br></br>
+            
+            <Link to="/games">Scores</Link><br></br>
+            <Link to="/play">Game</Link><br></br> 
+          </nav>
+
+          <Switch>
+            <Route path="/games"><APIContainer/></Route>
+            <Route path="/play"><GameContainer/></Route>
+            <Route path="/"><HowToPlay/></Route>
+          </Switch>
+
+        </div>
+
+      </Router>
+
   );
 }
 
--- pagan-emoji-land-client/src/containers/GameContainer.js
+++ pagan-emoji-land-client/src/containers/GameContainer.js
@@ -3,91 +3,58 @@ import '.././styles/GameContainer.css'
 import { connect } from 'react-redux';
 import ChoosePlayer from '../components/players/ChoosePlayer';
 import Spaces from '../containers/SpacesContainer'
-import HowToPlay from '../components/rules/HowToPlay';
 import Scores from '../components/impact/Scores'
-import APIContainer from './APIContainer';
-import { fetchAPI } from '../components/API/fetchGames';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link 
-} from 'react-router-dom'
+
 
 class GameContainer extends Component {
     
     render() {
         
     return (
-        <Router>
+       
             <div className="game-container">
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/games">Scores</Link>
-                        </li>
-                        <li>
-                            <Link to="/play">Game</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/games">
-                        <APIContainer />
-                    </Route>
-                    <Route path="/play"> 
-                        {
-                            (this.props.playerOne === "") 
-                            ? <ChoosePlayer addPlayer={this.props.addPlayer}/>
-                            :   <div className="spaces-container">
-                                    <Spaces 
-                                        playerOne={this.props.playerOne}
-                                        playerTwo={this.props.playerTwo}
-                                        playerThree={this.props.playerThree}
-                                        playerFour={this.props.playerFour}
-                                        elementName={this.props.elementName}
-                                        pOneCurrentPosition={this.props.pOneCurrentPosition}
-                                        pTwoCurrentPosition={this.props.pTwoCurrentPosition}
-                                        pThreeCurrentPosition={this.props.pThreeCurrentPosition}
-                                        pFourCurrentPosition={this.props.pFourCurrentPosition}
-                                        p1Impact={this.props.p1Impact}
-                                        p2Impact={this.props.p2Impact}
-                                        p3Impact={this.props.p3Impact}
-                                        p4Impact={this.props.p4Impact}
-                                        winning={this.props.winning}
-                                    />
-                                </div>
-                        }  
-                        {(this.props.playerOne !== "")
-                        ? <div className="scores-container"><Scores 
-                            playerOne={this.props.playerOne}
-                            playerTwo={this.props.playerTwo}
-                            playerThree={this.props.playerThree}
-                            playerFour={this.props.playerFour}
-                            p1Impact={this.props.p1Impact}
-                            p2Impact={this.props.p2Impact}
-                            p3Impact={this.props.p3Impact}
-                            p4Impact={this.props.p4Impact}
-                            winning={this.props.winning}/></div>
-                        :null
-                        }
-                    </Route>
-                    <Route path="/">
-                        <HowToPlay/>
-                    </Route>
-                </Switch>
-               
-                
-                   
-                    
-                    
-            
+                {
+                    (this.props.playerOne === "") 
+                    ? <ChoosePlayer addPlayer={this.props.addPlayer}/>
+                    :   <div className="spaces-container">
+                            <Spaces 
+                                playerOne={this.props.playerOne}
+                                playerTwo={this.props.playerTwo}
+                                playerThree={this.props.playerThree}
+                                playerFour={this.props.playerFour}
+                                elementName={this.props.elementName}
+                                pOneCurrentPosition={this.props.pOneCurrentPosition}
+                                pTwoCurrentPosition={this.props.pTwoCurrentPosition}
+                                pThreeCurrentPosition={this.props.pThreeCurrentPosition}
+                                pFourCurrentPosition={this.props.pFourCurrentPosition}
+                                p1Impact={this.props.p1Impact}
+                                p2Impact={this.props.p2Impact}
+                                p3Impact={this.props.p3Impact}
+                                p4Impact={this.props.p4Impact}
+                                winning={this.props.winning}
+                            />
+                        </div>
 
+                }  
+                        
+                { 
+                    (this.props.playerOne !== "")
+                    ? <div className="scores-container"><Scores 
+                        playerOne={this.props.playerOne}
+                        playerTwo={this.props.playerTwo}
+                        playerThree={this.props.playerThree}
+                        playerFour={this.props.playerFour}
+                        p1Impact={this.props.p1Impact}
+                        p2Impact={this.props.p2Impact}
+                        p3Impact={this.props.p3Impact}
+                        p4Impact={this.props.p4Impact}
+                        winning={this.props.winning}/></div>
+                    :null
+                }
+                    
             </div>
-        </Router>
+
+       
         )
     }
 }
@@ -112,6 +79,7 @@ const mapStateToProps = state => {
     }
 
 }
+
 
 
 
