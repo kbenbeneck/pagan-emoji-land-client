d80a202b9c21b53152be5941e3459cbabe292302
KBENBENECK <kbenbeneck@gmail.com>
2021-04-01 18:16:26 -0400

    Update APIContainer.js
    APIContainer imports fetch actions from new directory.
    Also imports Search form.
    
    Games list is now passed the searchResults prop that is mapped to state,

--- pagan-emoji-land-client/src/containers/APIContainer.js
+++ pagan-emoji-land-client/src/containers/APIContainer.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchGames } from '../components/API/fetchGames'
+import { fetchGames } from '../actions/fetchGames'
 import GamesList from '../components/API/GamesList'
+import SearchInput from '../components/search/SearchInput'
+
+
 
 
 class APIContainer extends Component {
     componentDidMount() {
         console.log(this.props)
+        console.log('a')
         this.props.fetchGames()
+        console.log('g')
     }
+    
     render() {
 
         console.log(this.props.games)
         return (
             <div className="api-container">
+                <SearchInput></SearchInput>
                 
-                <GamesList games={this.props.games}/>
+                <GamesList games={this.props.searchResults}
+                    searchResults={this.props.searchResults}
+                />
             </div>
         )
     }
@@ -23,6 +32,7 @@ class APIContainer extends Component {
 const mapStateToProps = state => {
     return {
         games: state.games,
+        searchResults: state.searchResults,
         loading: state.loading
     }
 }
@@ -31,4 +41,5 @@ const mapDispatchToProps = dispatch => {
         fetchGames: () => dispatch(fetchGames())
     }
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(APIContainer);
